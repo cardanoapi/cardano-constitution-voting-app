4c777f98974221bf311327c50cd673d287938cd3
Matthew Laux <mlaux6@outlook.com>
2024-10-21 09:39:19 -0500

    Add paths.ts file

---
+++ src/paths.ts
@@ -0,0 +1,8 @@
+// Use this file to define all the paths in the application
+export const paths = {
+  home: '/',
+  errors: { notFound: '/not-found' },
+  privacyPolicy: '/privacy-policy',
+  termsOfService: '/terms-of-service',
+  contact: '/contact',
+};
