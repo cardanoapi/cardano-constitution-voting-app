49b74bda903e1ffcbbdea483992e15a60a927195
Justin Schreiner <jschreiner22@gmail.com>
2024-10-23 14:21:15 -0600

    use name, unused variable on homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home(): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Home page</main>
+      <main>Home page {name}</main>
     </>
   );
 }
