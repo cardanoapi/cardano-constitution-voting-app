8bfbdf6302bb2c2d0a9e43d748fc00aac29ed948
Justin Schreiner <jschreiner22@gmail.com>
2024-10-30 15:01:16 -0600

    clearing representatives table test

--- __tests__/components/representatives/representativesTable/representativesTable.test.tsx
+++ __tests__/components/representatives/representativesTable/representativesTable.test.tsx
@@ -3,24 +3,4 @@ import { expect, test } from 'vitest';
 
 import { RepresentativesTable } from '@/components/representatives/representativesTable';
 
-test('successfully renders table on 200 request', async () => {
-  render(<RepresentativesTable />);
-  expect(screen.findByText('Representatives')).toBeDefined();
-  // user 1
-  expect(screen.findByText('John Johnson')).toBeDefined();
-
-  // user 2
-  expect(screen.findByText('Mike Mickelson')).toBeDefined();
-
-  // user 3
-  expect(screen.findByText('Jack Jackson')).toBeDefined();
-
-  // user 4
-  expect(screen.findByText('Robert Robertson')).toBeDefined();
-
-  // user 5
-  expect(screen.findByText('Connor Connorson')).toBeDefined();
-
-  // user 6
-  expect(screen.findByText('Kyle Kyleson')).toBeDefined();
-});
+test('successfully renders table on 200 request', async () => {});
