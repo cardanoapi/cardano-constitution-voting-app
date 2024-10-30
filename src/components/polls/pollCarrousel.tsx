fc69123d79942c684ee627d16c2289fa205822f9
Matthew Laux <mlaux6@outlook.com>
2024-10-30 14:26:31 -0600

    Remove unnecessary dependency on pollCarrousel

--- src/components/polls/pollCarrousel.tsx
+++ src/components/polls/pollCarrousel.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
@@ -32,7 +31,6 @@ export function PollCarrousel(): JSX.Element {
   }, []);
 
   const session = useSession();
-  const theme = useTheme();
 
   const pollCards = useMemo(() => {
     return (
@@ -83,7 +81,7 @@ export function PollCarrousel(): JSX.Element {
         </Box>
       </>
     );
-  }, [polls, theme.palette.text.primary]);
+  }, [polls]);
 
   if (loadingPolls) {
     return <></>;
