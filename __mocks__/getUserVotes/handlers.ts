046ba8c32daf5df1e910879b878c04ea85ee2484
Justin Schreiner <jschreiner22@gmail.com>
2024-10-30 13:34:44 -0600

    getUserVotes mocks

---
+++ __mocks__/getUserVotes/handlers.ts
@@ -0,0 +1,32 @@
+import { http, HttpResponse } from 'msw';
+
+export const getPollHandlers = [
+  http.get('/api/getUserVotes/*', () => {
+    return HttpResponse.json({
+      votes: [
+        {
+          poll_id: BigInt(1).toString(),
+          user_id: BigInt(1).toString(),
+          vote: 'yes',
+          signature: Date.now().toString(),
+          hashed_message: Date.now().toString(),
+        },
+        {
+          poll_id: BigInt(2).toString(),
+          user_id: BigInt(1).toString(),
+          vote: 'no',
+          signature: Date.now().toString(),
+          hashed_message: Date.now().toString(),
+        },
+        {
+          poll_id: BigInt(3).toString(),
+          user_id: BigInt(1).toString(),
+          vote: 'abstain',
+          signature: Date.now().toString(),
+          hashed_message: Date.now().toString(),
+        },
+      ],
+      message: 'Found user votes',
+    });
+  }),
+];
