a1dc290070d82deae5e1294928005d56e6885315
Matthew Laux <mlaux6@outlook.com>
2024-11-01 15:49:09 -0600

    Breakup pollStatusChip tests

--- __tests__/components/polls/pollStatusChip/pollStatusChip.test.tsx
+++
@@ -1,5 +0,0 @@
-import { test } from 'vitest';
-
-test('shows pending when the poll has not begun voting', async () => {});
-test('shows voting when the poll is open for voting', async () => {});
-test('shows finished when the poll has closed voting', async () => {});
