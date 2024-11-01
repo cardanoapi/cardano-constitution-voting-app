f26feb37bb8228a3457749b44f524eaa4b7e5e9f
Justin Schreiner <jschreiner22@gmail.com>
2024-11-01 10:57:20 -0600

    make get user vote axios

--- src/lib/getUserVote.tsx
+++ src/lib/getUserVote.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 /**
  * Gets a user's vote for a poll
  * @param userId - The ID of the user to fetch
@@ -8,10 +10,10 @@ export async function getUserVote(
   userId: string,
   pollId: string,
 ): Promise<{ vote: string; message: string }> {
-  const response = await fetch(`/api/getPollVote/${userId}/${pollId}`, {
+  const response = await axios.get(`/api/getPollVote/${userId}/${pollId}`, {
     headers: { 'X-Custom-Header': 'intersect' },
   });
-  const data = await response.json();
+  const data = await response.data;
 
   if (response.status === 200) {
     return { vote: data.vote, message: 'Vote found' };
