384aff8679090b9826dda987d6f72ff4b9d76433
Matthew Laux <mlaux6@outlook.com>
2024-11-01 17:47:28 -0600

    Backfill start vote tests

--- __tests__/components/buttons/beginVoteButton/alertsUserWhenPollIsNotFound.test.tsx
+++ __tests__/components/buttons/beginVoteButton/alertsUserWhenPollIsNotFound.test.tsx
@@ -1,12 +1,11 @@
 import { server } from '@/../__mocks__/server';
+import { startVotingNotFoundHandler } from '@/../__mocks__/startVoting/errorHandlers';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Toaster } from 'react-hot-toast';
 import { expect, test } from 'vitest';
 
 import { BeginVoteButton } from '@/components/buttons/beginVoteButton';
-
-import { startVotingNotFoundHandler } from '../../../../__mocks__/startVoting/errorHandlers';
 
 test('alerts user when poll is not found', async () => {
   server.use(...startVotingNotFoundHandler);
