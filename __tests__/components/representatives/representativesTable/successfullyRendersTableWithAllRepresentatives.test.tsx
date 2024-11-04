13f681804170fd2b1d1bfd87d61615239864052d
Matthew Laux <mlaux6@outlook.com>
2024-11-04 10:15:39 -0600

    Adjust representatives table tests

--- __tests__/components/representatives/representativesTable/successfullyRendersTableWithAllRepresentatives.test.tsx
+++ __tests__/components/representatives/representativesTable/successfullyRendersTableWithAllRepresentatives.test.tsx
@@ -7,15 +7,15 @@ test('successfully renders table with all representatives', async () => {
   render(<RepresentativesTable />);
   expect(await screen.findByText('Representatives')).toBeDefined();
   // user 1 -- use find all by text because he will show up 2 times in this table since he is the active voter
-  expect(await screen.findAllByText('John Johnson')).toBeDefined();
+  expect(await screen.findAllByText('John Johnson')).toHaveLength(2);
   // user 2
   expect(await screen.findByText('Mike Mickelson')).toBeDefined();
   // user 3 -- use find all by text because he will show up 2 times in this table since he is the active voter
-  expect(await screen.findAllByText('Jack Jackson')).toBeDefined();
+  expect(await screen.findAllByText('Jack Jackson')).toHaveLength(2);
   // user 4
   expect(await screen.findByText('Robert Robertson')).toBeDefined();
   // user 5 -- use find all by text because he will show up 2 times in this table since he is the active voter
-  expect(await screen.findAllByText('Connor Connorson')).toBeDefined();
+  expect(await screen.findAllByText('Connor Connorson')).toHaveLength(2);
   // user 6
   expect(await screen.findByText('Kyle Kyleson')).toBeDefined();
 });
