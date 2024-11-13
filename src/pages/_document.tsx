314e4d20e33313e63197c1be720bc7070b341beb
Justin Schreiner <jschreiner22@gmail.com>
2024-11-13 12:46:39 -0700

    add font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,14 @@ export default function Document(): JSX.Element {
       <CssBaseline enableColorScheme />
       <Html lang="en">
         <Head />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+        />
         <body>
           <Main />
           <NextScript />
