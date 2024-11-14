2f8597926a14ffd70b3411b2ebadfa9cd0bf5ace
Justin Schreiner <43378478+justinschreiner@users.noreply.github.com>
2024-11-14 16:55:47 -0700

    Update src/lib/checkIfVoting.tsx
    Co-authored-by: Matt Laux <86170323+mattlaux@users.noreply.github.com>

--- src/lib/checkIfVoting.tsx
+++ src/lib/checkIfVoting.tsx
@@ -2,8 +2,6 @@ import { prisma } from '@/db';
 
 /**
  * Checks if there are any actively voting polls
- * @param req - NextApiRequest
- * @param res - NextApiResponse
  * @returns Boolean - True if a poll is actively voting, false otherwise
  */
 export async function checkIfVoting(): Promise<boolean> {
