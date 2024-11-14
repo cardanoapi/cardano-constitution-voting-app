81572d78db2678f7aaff46ae0c8084c415838e2e
Niraj <duwalkneerose@gmail.com>
2024-11-14 15:07:32 +0545

    chore: add function to create new page with wallet and storageState

---
+++ integration_test/lib/helpers/page.ts
@@ -0,0 +1,32 @@
+import { importWallet } from '@fixtures/importWallet';
+import loadDemosExtension from '@fixtures/loadExtension';
+import { Browser, Page } from '@playwright/test';
+import { StaticWallet } from '@types';
+
+interface NewPageConfig {
+  storageState?: string;
+  wallet: StaticWallet;
+  enableStakeSigning?: boolean;
+  supportedExtensions?: Record<string, number>[];
+}
+
+export async function createNewPageWithWallet(
+  browser: Browser,
+  newPageConfig: NewPageConfig
+): Promise<Page> {
+  const { storageState, wallet, ...extensionConfig } = newPageConfig;
+
+  const context = await browser.newContext({
+    storageState,
+  });
+  const newPage = await context.newPage();
+
+  await loadDemosExtension(
+    newPage,
+    extensionConfig.enableStakeSigning,
+    extensionConfig.supportedExtensions
+  );
+  await importWallet(newPage, wallet);
+
+  return newPage;
+}
