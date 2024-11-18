d1cf13be6debc31012922df8a052cf71532942f6
Niraj <duwalkneerose@gmail.com>
2024-11-18 10:27:14 +0545

    chore: increase global timeout

--- integration_test/playwright.config.ts
+++ integration_test/playwright.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  timeout: 60_000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!environments.ci,
   /* Retry on CI only */
