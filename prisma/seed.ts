6efa4200636e9c6fb4218e5088324556f7ab8069
Niraj <duwalkneerose@gmail.com>
2024-11-18 11:16:52 +0545

    fix: seeding duplicate email issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -96,7 +96,7 @@ const users = [
     is_alternate: false,
     workshop_id: BigInt(5),
     name: 'Integration Delegate 2',
-    email: 'delegate1@integration.test',
+    email: 'delegate2@integration.test',
     wallet_address:
       'stake_test1uqxdv8wvdprs46el2r0p5gcyvz45nqkcz35dh4lwjjcvs9qltuf86',
   },
@@ -106,7 +106,7 @@ const users = [
     is_alternate: true,
     workshop_id: BigInt(5),
     name: 'Integration Alternate 2',
-    email: 'alternate1@integration.test',
+    email: 'alternate2@integration.test',
     wallet_address:
       'stake_test1uqltqgz58u4e0mnt4u2ttnn82ss3sq7d9an6xh49hajz0egghy55y',
   },
@@ -116,7 +116,7 @@ const users = [
     is_alternate: false,
     workshop_id: BigInt(6),
     name: 'Integration Delegate 3',
-    email: 'delegate2@integration.test',
+    email: 'delegate3@integration.test',
     wallet_address:
       'stake_test1uzn5n0yavvqvp2jhm5gc28a7kyh2nkqsrayu53m97yx7rfqk3qe0g',
   },
@@ -126,7 +126,7 @@ const users = [
     is_alternate: true,
     workshop_id: BigInt(6),
     name: 'Integration Alternate 3',
-    email: 'alternate2@integration.test',
+    email: 'alternate3@integration.test',
     wallet_address:
       'stake_test1uz8nudr07gyltqapgzvx9avyxcwp3cxnnlmgfd35px5jfgcq2hezv',
   },
