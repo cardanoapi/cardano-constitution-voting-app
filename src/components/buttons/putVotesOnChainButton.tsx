559341e504daf48dc446d132c5ac1c5c1c5cec6f
Matthew Laux <mlaux6@outlook.com>
2024-11-20 18:39:36 -0600

    Improve error handling of TX signing

--- src/components/buttons/putVotesOnChainButton.tsx
+++ src/components/buttons/putVotesOnChainButton.tsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react';
 import toast from 'react-hot-toast';
 
 import { awaitTxConfirm } from '@/lib/awaitTxConfirm';
+import { dismissToast } from '@/lib/dismissToast';
 import { addTxToPoll } from '@/lib/helpers/addTxToPoll';
 import { addTxToPollTransactions } from '@/lib/helpers/addTxToPollTransactions';
 import { addTxToPollVotes } from '@/lib/helpers/addTxToPollVotes';
@@ -35,81 +36,125 @@ export function PutVotesOnChainButton(props: Props): JSX.Element {
       toast.error('Invalid pollId');
       return;
     }
+
     setIsSubmitting(true);
+    let voteTxsUploaded = false;
+
     // Put votes on-chain
     const response = await getVotesTxMetadata(pollId);
     if (response.metadata) {
-      // For loop required as they metadata may be broken up into multiple transactions
-      for (const metadata of Object.values(response.metadata)) {
-        let success = false;
-        // Keep trying until the transaction is successful
-        while (!success) {
-          // Post votes on chain
-          const txResult = await postVotesOnChain(metadata.metadata);
-          setIsTxUploading(true);
-          if (txResult) {
-            // TODO: Figure out how to handle errors in addTxToPollTransactions and addTxToPollVotes since the data is already on-chain
-            success = true;
-            // Add txHash to poll
-            const addTxResponse = await addTxToPollTransactions(
-              pollId,
-              txResult.submitTxId,
-            );
-            if (addTxResponse.pollTransactionId === '-1') {
-              toast.error(addTxResponse.message);
-              break; // TODO: Do we really want to break here?
-            }
-            // Add txHash to poll votes
-            const addTxToVotesResponse = await addTxToPollVotes(
-              metadata.userIds,
-              pollId,
-              addTxResponse.pollTransactionId,
-            );
-            if (!addTxToVotesResponse.succeeded) {
-              toast.error(addTxToVotesResponse.message);
-              break; // TODO: Do we really want to break here?
-            }
+      // For loop required as the metadata may be broken up into multiple transactions
+      for (let i = 0; i < Object.keys(response.metadata).length; i++) {
+        const metadata = response.metadata[i];
+        // Post votes on chain
+        const txResult = await postVotesOnChain(metadata.metadata);
+        setIsTxUploading(true);
+        if (txResult) {
+          // Add txHash to poll_transaction records
+          const addTxResponse = await addTxToPollTransactions(
+            pollId,
+            txResult.submitTxId,
+          );
+          if (addTxResponse.pollTransactionId === '-1') {
+            // If there is an error, stop the loop and display an error message. This will allow
+            // the CO to manually upload the TX ID to the DB before moving onto the next TXs.
+            dismissToast(`Error saving the TX ${txResult.submitTxId} to the database. Please retrieve the TX ID
+              and metadata from your wallet transactions and manually upload to the
+              poll transactions in the DB.`);
+            setIsTxUploading(false);
+            break;
+          }
+          // Add txHash to poll votes
+          const addTxToVotesResponse = await addTxToPollVotes(
+            metadata.userIds,
+            pollId,
+            addTxResponse.pollTransactionId,
+          );
+          if (!addTxToVotesResponse.succeeded) {
+            // If there is an error, stop the loop and display an error message. This will allow
+            // the CO to manually upload the TX ID to the DB before moving onto the next TXs.
+            dismissToast(`Error saving the TX ${txResult.submitTxId} to the database. Please retrieve the TX ID
+            and metadata from your wallet transactions and manually upload to the
+            poll votes in the DB.`);
+            setIsTxUploading(false);
+            break;
+          }
+          // Only show the TX uploading on-chain message if the TX ID was
+          // successfully saved to both the poll_transaction and poll_vote tables.
+          if (
+            addTxResponse.pollTransactionId !== '-1' &&
+            addTxToVotesResponse.succeeded
+          ) {
             await awaitTxConfirm(txResult.submitTxId).then(() =>
               setIsTxUploading(false),
             );
-          } else {
-            toast.error(
-              'Error posting this TX on-chain. Please try signing this TX again. If the issue persists, refresh the app and re-click the upload votes on-chain button.',
-            );
-            break;
           }
+          // If this is the last metadata object, set voteTxsUploaded to true
+          if (i === Object.keys(response.metadata).length - 1) {
+            voteTxsUploaded = true;
+          }
+        } else {
+          // If there is an error, stop the loop and display an error message. This will allow
+          // the CO to retry signing the TX.
+          toast.error(
+            'Error posting this TX on-chain. Please try signing this TX again. If the issue persists, refresh the app and re-click the upload votes on-chain button.',
+          );
+          setIsTxUploading(false);
+          break;
         }
       }
     } else if (
       response.message !== 'All votes have already been uploaded on-chain'
     ) {
       toast.error(response.message);
+      setIsTxUploading(false);
+    } else {
+      // If there is no metadata because all votes are already on-chain,
+      // do not display an error message. Just move onto the summary TX.
+      voteTxsUploaded = true;
     }
-    // Put summary TX on-chain
-    let summaryTxSuccess = false;
-    while (!summaryTxSuccess) {
-      const response = await getSummaryTxMetadata(pollId);
-      if (response.metadata) {
-        const txResult = await postVotesOnChain(response.metadata);
+
+    // We only want to move onto the summary TX signing if all votes have been uploaded on-chain.
+    if (voteTxsUploaded) {
+      // Put summary TX on-chain
+      const summaryTxResponse = await getSummaryTxMetadata(pollId);
+      if (summaryTxResponse.metadata) {
+        const txResult = await postVotesOnChain(summaryTxResponse.metadata);
         setIsTxUploading(true);
         if (txResult) {
-          // TODO: Figure out how to handle errors in addTxToPoll since the data is already on-chain
-          summaryTxSuccess = true;
           // Add txHash to poll
           const addTxResponse = await addTxToPoll(pollId, txResult.submitTxId);
           if (!addTxResponse.success) {
-            toast.error(addTxResponse.message);
-            break;
+            // Note that if saving the TX ID to the DB fails, the user will have to manually upload the TX ID to the DB.
+            // The upload votes on-chain button will also reappear. However, the CO does not want to re-click this button
+            // as the TX is already on-chain. Once the TX ID is manually added to the DB, the button will disappear.
+            dismissToast(`Error saving the TX ${txResult.submitTxId} to the database. Please retrieve the TX ID
+              and metadata from your wallet transactions and manually upload to the
+              poll in the DB.`);
+            setIsTxUploading(false);
+          } else {
+            // Only show the TX uploading on-chain message if the TX ID was
+            // successfully saved to both the poll_transaction and poll_vote tables.
+            await awaitTxConfirm(txResult.submitTxId).then(() =>
+              setIsTxUploading(false),
+            );
           }
-          await awaitTxConfirm(txResult.submitTxId).then(() =>
-            setIsTxUploading(false),
+        } else {
+          // If there is an error, display an error message. This will allow
+          // the CO to retry signing the TX.
+          toast.error(
+            'Error posting this TX on-chain. Please try signing this TX again. If the issue persists, refresh the app and re-click the upload votes on-chain button.',
           );
+          setIsTxUploading(false);
         }
       } else {
+        // If there is no metadata display an error message.
+        // The user will have to try signing the TX again.
         toast.error(response.message);
-        break;
+        setIsTxUploading(false);
       }
     }
+
     setIsSubmitting(false);
   }
 
---
+++ src/lib/dismissToast.tsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import toast from 'react-hot-toast';
+
+export function dismissToast(text: string): void {
+  toast.error(
+    (t) => (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="flex-start"
+        alignContent="flex-start"
+      >
+        <div
+          style={{
+            wordWrap: 'break-word',
+            whiteSpace: 'normal',
+            maxWidth: '300px',
+          }}
+        >
+          {text}
+        </div>
+        <Button
+          onClick={() => toast.dismiss(t.id)} // Close the toast programmatically
+          style={{
+            marginLeft: '10px',
+            background: 'red',
+            color: 'white',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          Dismiss
+        </Button>
+      </Box>
+    ),
+    {
+      duration: Infinity,
+    },
+  );
+}
